6df9988c009e82e5048199b7501d187ebad6db79
Matthew Marcos <matthewmarcos94@gmail.com>
2018-09-18 16:26:13 +0800

    Basic Endgame conditions

--- core/src/com/matthewmarcos/spacerocks/Explosion.java
+++ core/src/com/matthewmarcos/spacerocks/Explosion.java
@@ -6,5 +6,13 @@ public class Explosion extends BaseActor {
 
     public Explosion(float x, float y, Stage s) {
         super(x, y, s, "Explosion");
+        loadAnimationFromSheet("explosion.png", 6, 6, 0.03f, false);
+    }
+
+    public void act(float dt) {
+        super.act(dt);
+        if(isAnimationFinished()) {
+            remove();
+        }
     }
 }
--- core/src/com/matthewmarcos/spacerocks/LevelScreen.java
+++ core/src/com/matthewmarcos/spacerocks/LevelScreen.java
@@ -1,9 +1,11 @@
 package com.matthewmarcos.spacerocks;
 
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 
 public class LevelScreen extends BaseScreen {
     private SpaceShip spaceship;
+    private boolean gameOver;
 
     public void initialize() {
         BaseActor space = new BaseActor(0, 0, mainStage);
@@ -21,10 +23,64 @@ public class LevelScreen extends BaseScreen {
         new Rock(400, 500, mainStage);
 
         spaceship = new SpaceShip(400, 300, mainStage);
+
+        gameOver = false;
     }
 
     public void update(float dt) {
 
+        generateRocks();
+
+        for(BaseActor rockActor : BaseActor.getList(mainStage, "Rock")) {
+            if(rockActor.overlaps(spaceship)) {
+                if(spaceship.shieldPower <= 0) {
+                    gameOver();
+                }
+                else {
+                    spaceship.shieldPower -= 34;
+                    Explosion boom = new Explosion(0, 0, mainStage);
+                    boom.centerAtActor(rockActor);
+                    rockActor.remove();
+                }
+            }
+
+            for(BaseActor laserActor : BaseActor.getList(mainStage, "Laser")) {
+                if(laserActor.overlaps(rockActor)) {
+                    Explosion boom = new Explosion(0, 0, mainStage);
+                    boom.centerAtActor(rockActor);
+                    laserActor.remove();
+                    rockActor.remove();
+                }
+            }
+        }
+
+        if(!gameOver && BaseActor.count(mainStage, "Rock") == 0) {
+            BaseActor messageWin = new BaseActor(0, 0, uiStage);
+            messageWin.loadTexture("message-win.png");
+            messageWin.centerAtPosition(400, 300);
+            messageWin.setOpacity(0);
+            messageWin.addAction(Actions.fadeIn(1));
+            gameOver = true;
+        }
+    }
+
+    public void generateRocks() {
+
+    }
+
+    private void gameOver() {
+        gameOver = true;
+
+        Explosion boom = new Explosion(0, 0, mainStage);
+        boom.centerAtActor(spaceship);
+        spaceship.remove();
+        spaceship.setPosition(-1000, -1000);
+
+        BaseActor gameOver = new BaseActor(0, 0, uiStage);
+        gameOver.centerAtPosition(200, 300);
+        gameOver.setOpacity(0);
+        gameOver.addAction(Actions.fadeIn(1));
+        gameOver.loadTexture("message-lose.png");
     }
 
     public boolean keyDown(int keycode) {
